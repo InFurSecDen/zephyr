1f951b09418e4aa998bc998d22195a98767a1b33
Opal Symes <code@opal.codes>
2018-11-13 21:41:37 +1300

    oops, forgot one

--- boards/arm/nrf51_eysgcnzwy/board.h
+++ boards/arm/nrf51_eysgcnzwy/board.h
@@ -11,7 +11,7 @@
 
 /* Push button switch 0 KEY1 */
 #define SW0_GPIO_PIN     17
-#define SW0_GPIO_NAME    CONFIG_GPIO_NRF5_P0_DEV_NAME
+#define SW0_GPIO_NAME    CONFIG_GPIO_NRF_P0_DEV_NAME
 #define SW0_GPIO_PIN_PUD GPIO_PUD_PULL_UP
 
 #endif /* __INC_BOARD_H */
